930f03e6921f169c38dcfb67234bc2b48777a783
springles <hecacho@naver.com>
2023-06-26 20:38:55 +0900

    nomad flutter ~#6.4, 기본생성자

---
+++ lib/models/webtoon_model.dart
@@ -0,0 +1,7 @@
+class webtoon_model{
+  String id;
+  String title;
+  String thumb;
+
+  webtoon_model({required this.id, required this.title, required this.thumb});
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,19 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import '../models/webtoon_model.dart';
 
 class Apiservice{
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   void getTodaysToons() async{
+    List<webtoon_model> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     final List webtoons = jsonDecode(response.body);
     for (var webtoon in webtoons) {
-      print(webtoon);
+      webtoonInstances.add(webtoon_model(id: webtoon['id'], title: webtoon['title'], thumb: webtoon['thumb']));
     }
+    print(webtoonInstances[3].title);
   }
 }
